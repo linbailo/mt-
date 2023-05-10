ee0c7502845baf7bc666be572bc4a3cc3618e0fe
linlin <1628708538@qq.com>
2023-05-10 13:22:57 +0800

    Add files via upload

--- lin.py
+++ lin.py
@@ -3,10 +3,10 @@ mt论坛自动签到
 
 暂不支持多个账号，我知道你们肯定没有这个需求
 
-账号变量 username 
-密码变量 password
-export username=""
-export password=""
+账号变量 mtusername 
+密码变量 mtpassword
+export mtusername=""
+export mtpassword=""
 
 cron: 0 0,7, * * *
 const $ = new Env("mt论坛");
@@ -26,9 +26,9 @@ def main(username,password):
     headers={'User-Agent': ua}
     session.get('https://bbs.binmt.cc/member.php?mod=logging&action=login&infloat=yes&handlekey=login&inajax=1&ajaxtarget=fwin_content_login',headers=headers)
     chusihua = session.get('https://bbs.binmt.cc/member.php?mod=logging&action=login&infloat=yes&handlekey=login&inajax=1&ajaxtarget=fwin_content_login',headers=headers)
-    print(re.findall('loginhash=(.*?)">', chusihua.text)
+    print(re.findall('loginhash=(.*?)">', chusihua.text))
     loginhash = re.findall('loginhash=(.*?)">', chusihua.text)[0]
-    formhash = re.findall('formhash" value="(.*?)".*? />', chusihua.text, re.S)[0]
+    formhash = re.findall('formhash" value="(.*?)".*? />', chusihua.text)[0]
     denurl = f'https://bbs.binmt.cc/member.php?mod=logging&action=login&loginsubmit=yes&handlekey=login&loginhash={loginhash}&inajax=1'
     data = {'formhash': formhash,'referer': 'https://bbs.binmt.cc/forum.php','loginfield': 'username','username': username,'password': password,'questionid': '0','answer': '',}
     denlu = session.post(headers=headers, url=denurl, data=data).text
@@ -39,7 +39,7 @@ def main(username,password):
         print(f'{fzmz}：登录成功')
         #获取formhash
         zbqd = session.get('https://bbs.binmt.cc/k_misign-sign.html', headers=headers).text
-        formhash = re.findall('formhash" value="(.*?)".*? />', zbqd, re.S)[0]
+        formhash = re.findall('formhash" value="(.*?)".*? />', zbqd)[0]
         #签到
         qdurl=f'https://bbs.binmt.cc/plugin.php?id=k_misign:sign&operation=qiandao&format=text&formhash={formhash}'
         qd = session.get(url=qdurl, headers=headers).text
@@ -52,18 +52,19 @@ def main(username,password):
 
 if __name__ == '__main__':
     #账号
-    username = ''
+    username = '林lin'
     #username.encode("utf-8")
     #密码
-    password = ''
-    if 'username' in os.environ:
-        username = os.environ.get("username")
+    password = '13977922831'
+    if 'mtusername' in os.environ:
+        username = os.environ.get("mtusername")
     else:
         print('不存在青龙、github变量')
-    if 'password' in os.environ:
-        password = os.environ.get("password")
+    if 'mtpassword' in os.environ:
+        password = os.environ.get("mtpassword")
     
     try:
         main(username,password)
     except Exception as e:
         raise e
+
