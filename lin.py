f37362d4fa06492e1fb8d5d1df4e7fb37e1bf38f
linlin <1628708538@qq.com>
2023-05-11 15:18:54 +0800

    Update lin.py

--- lin.py
+++ lin.py
@@ -2,13 +2,12 @@
 mt论坛自动签到
 
 暂不支持多个账号，我知道你们肯定没有这个需求
+添加变量mtluntan
+账号密码用&隔开
+例如账号：10086 密码：1001 则变量为10086&1001
+export mtluntan=""
 
-账号变量 mtusername 
-密码变量 mtpassword
-export mtusername=""
-export mtpassword=""
-
-cron: 0 0,7, * * *
+cron: 0 0,7 * * *
 const $ = new Env("mt论坛");
 """
 import requests
@@ -17,22 +16,23 @@ import os
 import time
 
 #qq:1628708538
-requests.urllib3.disable_warnings()
+
 #设置ua
 ua = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36'
 session = requests.session()
 
+
 def main(username,password):
     headers={'User-Agent': ua}
-    #session.get('https://bbs.binmt.cc/member.php?mod=logging&action=login&infloat=yes&handlekey=login&inajax=1&ajaxtarget=fwin_content_login',headers=headers)
-    chusihua = session.get('https://bbs.binmt.cc/member.php?mod=logging&action=login&infloat=yes&handlekey=login&inajax=1&ajaxtarget=fwin_content_login',headers=headers,proxies={"http": "http://36.6.145.111:8089"})
-    print(chusihua.text)
-    #loginhash = re.findall('loginhash=(.*?)">', chusihua.text)[0]
+    session.get('https://bbs.binmt.cc/member.php?mod=logging&action=login&infloat=yes&handlekey=login&inajax=1&ajaxtarget=fwin_content_login',headers=headers)
+    chusihua = session.get('https://bbs.binmt.cc/member.php?mod=logging&action=login&infloat=yes&handlekey=login&inajax=1&ajaxtarget=fwin_content_login',headers=headers)
+    #print(re.findall('loginhash=(.*?)">', chusihua.text))
+    loginhash = re.findall('loginhash=(.*?)">', chusihua.text)[0]
     formhash = re.findall('formhash" value="(.*?)".*? />', chusihua.text)[0]
     denurl = f'https://bbs.binmt.cc/member.php?mod=logging&action=login&loginsubmit=yes&handlekey=login&loginhash={loginhash}&inajax=1'
     data = {'formhash': formhash,'referer': 'https://bbs.binmt.cc/forum.php','loginfield': 'username','username': username,'password': password,'questionid': '0','answer': '',}
     denlu = session.post(headers=headers, url=denurl, data=data).text
-    print(denlu)
+    #print(denlu)
     if '欢迎您回来' in denlu:
         #获取分组、名字
         fzmz = re.findall('欢迎您回来，(.*?)，现在', denlu)[0]
@@ -52,16 +52,20 @@ def main(username,password):
 
 if __name__ == '__main__':
     #账号
-
-    if 'username' in os.environ:
-        username = os.environ.get("username")
+    username = ''
+    #username.encode("utf-8")
+    #密码
+    password = ''
+    if 'mtluntan' in os.environ:
+        mtluntan = os.environ.get("mtluntan").split("&")
+        username = mtluntan[0]
+        password = mtluntan[1]
     else:
         print('不存在青龙、github变量')
-    if 'password' in os.environ:
-        password = os.environ.get("password")
-    
+        if username == '' or password == '':
+            print('本地账号密码为空')
+            exit()
     try:
         main(username,password)
     except Exception as e:
         raise e
-
